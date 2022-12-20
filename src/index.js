c71536cdfd9e28e70e05ee140601b1a7109e19c0
Florian <florianpaulschoene@gmail.com>
2022-12-20 22:04:39 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//  template to generate basic html
+//      -header
+//      -navBar
+//          - home (all tasks)
+//          - Today
+//          - Week
+//          - Projects
+//              -   Sport
+//              -   Study
+//          - Notes
+//
+//      -content
+//
+//  class to do
+//      -   name
+//      -   description
+//      -   date
+//      -   importance
+//      -   edit description & name
+//      -   delete
+//      -   project
+//
+// class Notes
+//      -   name
+//      -   note
